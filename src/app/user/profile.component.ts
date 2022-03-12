c8e6ab5e62d3f19e36d9034d773059db42b35255
tonythomasinfo <tthomasmanthuruthil@infomedia.com.au>
2022-03-12 13:43:50 +1100

    Valdators added for component driven form.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -15,6 +15,8 @@ import {Router} from '@angular/router';
 })
 export class ProfileComponent implements OnInit{
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(private authService: AuthService, private router: Router) {
 
@@ -23,11 +25,13 @@ export class ProfileComponent implements OnInit{
   // Model driven
   // This is for component to html data sending.
   ngOnInit(): void {
-    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
-    let lastName =  new FormControl(this.authService.currentUser.lastName, Validators.required);
+     this.firstName = new FormControl(this.authService.currentUser.firstName,
+      [Validators.required, Validators.pattern('[a-zA-Z].*')]);
+     this.lastName =  new FormControl(this.authService.currentUser.lastName,
+      [Validators.required, Validators.pattern('[a-zA-Z].*')]);
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName:  this.lastName
     });
   }
 
@@ -40,5 +44,13 @@ export class ProfileComponent implements OnInit{
       this.authService.updateCurrentuser(formValues.firstName, formValues.lastName);
       this.router.navigate(['events']);
     }
-    }
+  }
+
+  validateFirstName(){
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.untouched;
+  }
 }
